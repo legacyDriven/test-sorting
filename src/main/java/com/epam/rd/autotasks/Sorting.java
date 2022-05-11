34bc26efcd5374e16ea2cf47e24ebdaa207d3f12
Eugeniusz <eugenioskye@gmail.com>
2022-05-11 10:34:42 +0200

    bubblesort impl, first commit

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,25 @@
 package com.epam.rd.autotasks;
 
 public class Sorting {
+
+    public Sorting() {
+    }
+
     public void sort(int[] array){
-        throw new UnsupportedOperationException();
+        if (array==null) throw new IllegalArgumentException("Can not sort null reference");
+
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,35 +1,71 @@
 package com.epam.rd.autotasks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.Assert.*;
 
 public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testNullCase(){
-
+//        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+//            Sorting s = new Sorting();
+//            s.sort(null);
+//        });
+//
+//        String expectedMessage = "Can not sort null reference";
+//        String actualMessage = exception.getMessage();
+//        assertTrue(actualMessage.contains(expectedMessage));
+        Sorting sort = new Sorting();
+        sort.sort(null);
     }
 
     @Test
     public void testEmptyCase(){
+        Sorting sort = new Sorting();
 
+        int[] input = new int[0];
+        sort.sort(input);
+        int[] expected = new int[0];
+
+        assertEquals(Arrays.toString(expected), Arrays.toString(input));
     }
 
     @Test
     public void testSingleElementArrayCase() {
+        Sorting sort = new Sorting();
+        int [] input = {5};
+        sort.sort(input);
+        int[] expected = {5};
 
+        assertEquals(Arrays.toString(expected), Arrays.toString(input));
     }
 
     @Test
     public void testSortedArraysCase() {
-
+        int[] input = {3,5,4,1,2};
+        int[] expected = {1,2,3,4,5};
+        sorting.sort(input);
+        assertEquals(Arrays.toString(expected), Arrays.toString(input));
     }
 
     @Test
     public void testOtherCases() {
-
+        int[] input = new int[1000];
+        for(int i = 0; i < input.length; i++){
+            input[i] = ThreadLocalRandom.current().nextInt(5000);
+        }
+        int[] expected = new int [1000];
+                System.arraycopy(input,0, expected, 0, 1000 );
+        sorting.sort(input);
+        Arrays.sort(expected);
+        assertEquals(Arrays.toString(expected), Arrays.toString(input));
     }
 
 }
